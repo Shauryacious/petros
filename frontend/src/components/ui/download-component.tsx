29e86ab8ba85602bee94edc5f4c38580c71e838d
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-10-26 12:46:08 +0530

    uploaded the code

---
+++ frontend/src/components/ui/download-component.tsx
@@ -0,0 +1,56 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import { isWindows, isMacOs } from 'react-device-detect';
+
+const DownloadComponent = () => {
+  const [platform, setPlatform] = useState(null);
+
+  useEffect(() => {
+    if (isWindows) {
+      setPlatform('windows');
+    } else if (isMacOs) {
+      setPlatform('mac');
+    } else {
+      setPlatform('unsupported');
+    }
+  }, []); // Runs only once after component mounts
+
+  return (
+    <div>
+      {platform === 'windows' && (
+        <div>
+          <div
+           
+            style={{ color: 'black' }}
+          >
+            <i
+              className="fa-brands fa-windows"
+              style={{ paddingRight: '20px', color: 'black' }}
+            ></i>
+            Classify Your Rock
+          </div>
+        </div>
+      )}
+      {platform === 'mac' && (
+        <div>
+          <div
+           
+            target="_blank"
+            style={{ color: 'black' }}
+          >
+            <i
+              className="fa-brands fa-apple"
+              style={{ paddingRight: '20px', color: 'black' }}
+            ></i>
+           Classify Your Rock
+          </div>
+        </div>
+      )}
+      {platform === 'unsupported' && (
+        <div>Sorry, your operating system is not supported for download.</div>
+      )}
+    </div>
+  );
+};
+
+export default DownloadComponent;
