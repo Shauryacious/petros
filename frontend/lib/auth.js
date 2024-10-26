36b22231305898bf2f9e14b7d5a931f2a094af4c
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-10-26 12:39:12 +0530

    created a default next app

---
+++ frontend/lib/auth.js
@@ -0,0 +1,13 @@
+// import {NextAuthOptions,User,getServerSession} from 'next-auth'
+// export const authConfig: NextAuthOptions = {
+//     providers: ({
+//         CredentialsProvider({
+//             id:"credentials",
+//             name:"Credentials",
+//             credentials:{
+//                 email:{label:"Email",type:"email",placeholder:"Enter your email"},
+//                 password:{label:"Password",type:"password",placeholder:"Enter your password"}
+//             },
+//         })
+//     }),
+// };
---
+++ frontend/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com'], // Add other domains as necessary
+      },
+};
+
+export default nextConfig;
