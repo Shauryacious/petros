36a6a66a1bb62d93a77888e7cb3a9972e67e1fe0
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-10-26 12:48:31 +0530

    integrated spline

---
+++ frontend/src/app/spline/page.tsx
@@ -0,0 +1,12 @@
+
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+  return (
+    <main style={{position:'relative',top:"-40px"}}>
+      <Spline
+         scene="https://prod.spline.design/GQOwcbMhfOB4YJYw/scene.splinecode"
+      />
+    </main>
+  );
+}
