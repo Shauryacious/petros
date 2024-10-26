e410103c2a5cede66d2e524ce5c41b5e155aa586
Shauryacious <shauryaagrawal2718@gmail.com>
2024-10-26 11:48:15 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-// simple server
+// visioniyam/backend/server.js
+const dotenv = require("dotenv");
+const path = require('path');
+
+const app = require("./app");
+const connectDB = require('./configuration/connectDB');
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 3001;
+const server = app.listen(port, () => {
+    console.log(`App is running on port ${port} in '${process.env.NODE_ENV}' mode`);
+});
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Radhe Radhe!");
+});
