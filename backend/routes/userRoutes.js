812538cc377c4e2ce51891f8a4e2812e7257af3a
Shauryacious <shauryaagrawal2718@gmail.com>
2024-10-26 12:01:14 +0530

    Setting Up User Routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+// visioniyam/backend/routes/userRoutes.js
+const express = require("express");
+const router = express.Router();
+
+const authController = require("./../controllers/authController");
+
+// User signup and login routes
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+
+// Update the create route to handle multiple images
+router.post('/create', authController.upload.array('images', 1), authController.createPerson);
+router.post('/buy', authController.buy);
+
+router.post('/forgetPassword', authController.forgetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.post('/makerequest', authController.makerequest);
+router.post('/validation', authController.validation);
+
+module.exports = router;
